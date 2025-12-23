1d57a74278c8763a6152e0e59a401a89399a6009
Murillo-test <murillo.100@hotmail.com>
2025-12-23 00:29:50 +0000

    aula 22-12-25 inicio

---
+++ variaveis.py
@@ -0,0 +1,8 @@
+nome = input("Digite o seu nome: ")
+idade = input("Digite a sua idade: ")
+
+# input permite ler do teclado. O comando vai
+# igualar a variável ao valor digitado e vai
+# permitir imprimir a variável atribuída.
+
+print(f"Meu nome é {nome} e eu tenho {idade} anos de idade.")
---
+++ variaveis2.py
@@ -0,0 +1,6 @@
+nome1 = "Murillo"
+nome2 = "Cardoso"
+nome3 = "Rodrigues"
+nome4 = "Pereira"
+
+print(f"Meu nome é {nome1} {nome2} {nome3} {nome4}")
