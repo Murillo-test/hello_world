76daabfecee61582d6fd1615719a78b274475179
Murillo-test <murillo.100@hotmail.com>
2026-01-06 22:07:06 +0000

    lista de ferias

---
+++ 11_listaexwpp02.py
@@ -0,0 +1,25 @@
+#nome = input("Por favor, informe seu nome: ")
+#print(f"Olá, {nome}!")
+
+# ex 02
+
+#logradouro = input("Por favor, informe o logradouro: ")
+#numero = input("Certo, agora informe o número residencial: ")
+#cidade = input("Por fim, informe a cidade: ")
+#confirmacao = input(f"Confirma o endereço (S/N)? {logradouro}, {numero} - {cidade} : ")
+
+# EX 03
+
+# nome = input("Por favor, informe seu nome: ")
+# idade = input("Certo. Agora informe sua idade: ")
+# email = input("Por fim, informe seu e-mail: ")
+# print(" ")
+# print("-----------------")
+# print(f"Nome: {nome}")
+# print(f"Idade: {idade}")
+# print(f"E-mail: {email}")
+# print("-----------------")
+# print(" ")
+
+# EX 04
+
