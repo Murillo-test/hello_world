1ca63adf0469182150f37d411edad0521616a47e
Murillo-test <murillo.100@hotmail.com>
2026-01-07 01:04:33 +0000

    aula do dia 06-01-2026

---
+++ 12_condicionantes.py
@@ -0,0 +1,58 @@
+#maioridade_penal = 18
+#idade_idoso = 60
+#idade = 21
+
+#print((idade > maioridade_penal) and (idade < idade_idoso))
+
+# outro:
+
+#maioridade_penal = 18
+#idade = int(input("Digite sua idade: "))
+
+#if (idade >= maioridade_penal):
+#    print("Pode beber à vontade!")
+#else:
+#    print("Não pode beber.")
+
+#pos = input().split(" ")
+#pos_P = pos[0]
+#pos_R = pos[1]
+
+#if (pos_P == "1"):
+#    if (pos_R == "0"):
+#        print("B")
+#    else:
+#        print("A")
+#else:
+#    print("C")
+
+# ELIF é um comando que junta else seguido de if
+
+
+
+
+nmr = int(input())
+if (nmr == 1):
+    print("January")
+elif (nmr == 2):
+    print("February")
+elif (nmr == 3):
+    print("March")
+elif (nmr == 4):
+    print("April")
+elif (nmr == 5):
+    print("May")
+elif (nmr == 6):
+    print("June")
+elif (nmr == 7):
+    print("July")
+elif (nmr == 8):
+    print("August")
+elif (nmr == 9):
+    print("September")
+elif (nmr == 10):
+    print("October")
+elif (nmr == 11):
+    print("November")
+else:
+    print("December")
