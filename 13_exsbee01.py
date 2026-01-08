bec86219fbde7f3f840f21f3fb55aa54b05b3a3e
Murillo-test <murillo.100@hotmail.com>
2026-01-08 01:18:59 +0000

    final treino 07-01-26

--- 13_exsbee01.py
+++ 13_exsbee01.py
@@ -29,3 +29,98 @@
 #B = int(input())
 #SOMA = A + B
 #print(f"SOMA = {SOMA}")
+
+#1004
+
+#A = int(input())
+#B = int(input())
+#PROD = A*B
+#print(f"PROD = {PROD}")
+
+#1005
+
+#A = float(input())
+#B = float(input())
+#MEDIA = (A*0.35+B*0.75)/1.1
+#print(f"MEDIA = {MEDIA:.5f}")
+
+#1006
+
+#A = float(input())
+#B = float(input())
+#C = float(input())
+#MEDIA = (A*2+B*3+C*5)/10
+#print(f"MEDIA = {MEDIA:.1f}")
+
+#1007
+
+#A = int(input())
+#B = int(input())
+#C = int(input())
+#D = int(input())
+#dif = A*B-C*D
+#print(f"DIFERENCA = {dif}")
+
+#1008
+
+#nr = int(input())
+#hrs = int(input())
+#phr = float(input())
+#slr = hrs*phr
+#print(f"NUMBER = {nr}")
+#print(f"SALARY = U$ {slr:.2f}")
+
+#1009
+
+#seller = input()
+#sal = float(input())
+#sold = float(input())
+#tot = sal+sold*0.15
+#print(f"TOTAL = R$ {tot:.2f}")
+
+#1010
+
+#pr1 = input().split()
+#cpr1 = int(pr1[0])
+#npr1 = int(pr1[1])
+#valpr1 = float(pr1[2])
+
+#pr2 = input().split()
+#cpr2 = int(pr2[0])
+#npr2 = int(pr2[1])
+#valpr2 = float(pr2[2])
+
+#tot = npr1*valpr1+npr2*valpr2
+
+#print(f"VALOR A PAGAR: R$ {tot:.2f}")
+
+#1011
+
+#pi = float(3.14159)
+#R = float(input())
+#vol = (4.0/3)*pi*(R**3)
+#print(f"VOLUME = {vol:.3f}")
+
+#1012
+
+#lista = input().split()
+#A = float(lista[0])
+#B = float(lista[1])
+#C = float(lista[2])
+
+#tri = A*C/2
+
+#pi = float(3.14159)
+#circ = pi*(C**2)
+
+#trap = C*(A+B)/2
+
+#quad = B**2
+
+#ret = A*B
+
+#print(f"TRIANGULO: {tri:.3f}")
+#print(f"CIRCULO: {circ:.3f}")
+#print(f"TRAPEZIO: {trap:.3f}")
+#print(f"QUADRADO: {quad:.3f}")
+#print(f"RETANGULO: {ret:.3f}")
